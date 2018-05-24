4a8dab67db394cef7fd43f9b81979b3234d51034
Wuzzy <Wuzzy2@mail.ru>
2018-05-24 23:53:51 +0200

    Add desert sandstone deco nodes

--- nodes.lua
+++ nodes.lua
@@ -6,22 +6,36 @@ else
 	S = function(s) return s end
 end
 
-local desc = {S("Sandstone with Eye Engraving"), S("Sandstone with Man Engraving"), S("Sandstone with Sun Engraving")}
-local img = {"eye", "men", "sun"}
+local img = {
+	"eye", "men", "sun",
+	"scarab", "ankh", "cactus"
+}
+local desc = {
+	S("Sandstone with Eye Engraving"), S("Sandstone with Man Engraving"), S("Sandstone with Sun Engraving"),
+	S("Desert Sandstone with Scarab Engraving"), S("Desert Sandstone with Ankh Engraving"), S("Desert Sandstone with Cactus Engraving")
+}
 
 local decodesc = ""
 if minetest.get_modpath("doc_items") then
 	decodesc = doc.sub.items.temp.deco
 end
 
-for i=1,3 do
+for i=1, #img do
+	local sandstone_img, basenode
+	if i > 3 then
+		sandstone_img = "default_desert_sandstone.png"
+		basenode = "default:desert_sandstone"
+	else
+		sandstone_img = "default_sandstone.png"
+		basenode = "default:sandstone"
+	end
 	minetest.register_node("tsm_pyramids:deco_stone"..i, {
 		description = desc[i],
 		_doc_items_longdesc = decodesc,
-		tiles = {"default_sandstone.png", "default_sandstone.png", "default_sandstone.png^tsm_pyramids_"..img[i]..".png"},
 		is_ground_content = false,
-		groups = minetest.registered_nodes["default:sandstone"].groups,
-		sounds = default.node_sound_stone_defaults(),
+		tiles = {sandstone_img, sandstone_img, sandstone_img.."^tsm_pyramids_"..img[i]..".png"},
+		groups = minetest.registered_nodes[basenode].groups,
+		sounds = minetest.registered_nodes[basenode].sounds,
 	})
 end
 

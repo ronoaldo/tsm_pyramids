47f80863ba48154578a77d646ac7f0036fa1effb
Wuzzy <Wuzzy2@mail.ru>
2019-08-19 17:50:24 +0200

    Update translation support for Minetest 5.0.0

--- mummy.lua
+++ mummy.lua
@@ -1,10 +1,4 @@
--- Boilerplate to support localized strings if intllib mod is installed.
-local S
-if minetest.get_modpath("intllib") then
-	S = intllib.Getter()
-else
-	S = function(s) return s end
-end
+local S = minetest.get_translator("tsm_pyramids")
 
 local mummy_walk_limit = 1
 local mummy_chillaxin_speed = 1
